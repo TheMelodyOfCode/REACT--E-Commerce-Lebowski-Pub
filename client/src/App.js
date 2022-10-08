71ac4c09f6d3b6a69351917ab523cd3fe283a014
D.Cameron <lobbygow@posteo.de>
2022-10-08 11:53:21 +0200

    added db connection for categories

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,47 @@
 import './main.scss'
+import {useState, useEffect} from "react";
+import React from "react";
 
-const categories = [
-  {
-    id: 1,
-    title: 'Burger',
-  },
-  {
-    id: 2,
-    title: 'White Russian',
-  },
-  {
-    id: 3,
-    title: 'Bowling Balls',
-  },
-  {
-    id: 4,
-    title: 'Whiskey',
-  },
-  {
-    id: 5,
-    title: 'Rugs',
-  },
-]
+// const categories = [
+//   {
+//     id: 1,
+//     title: 'Burger',
+//   },
+//   {
+//     id: 2,
+//     title: 'White Russian',
+//   },
+//   {
+//     id: 3,
+//     title: 'Bowling Balls',
+//   },
+//   {
+//     id: 4,
+//     title: 'Whiskey',
+//   },
+//   {
+//     id: 5,
+//     title: 'Rugs',
+//   },
+// ]
 
 const App = () => {
+
+  const [categories, setCategories] = useState([]); // [value, setValue]
+
+  useEffect(()=>{
+    fetch('http://localhost:3001/categories')
+    .then((response)=> { return response.json();})
+    .then((categories=> { setCategories(categories);}));
+  },[])
+
   return (
     <section className="section-categories">
       <div className="categories">
 
-        {categories.map(({title, id, image}) => (
+        {categories.map(({id, title, img, imgSm}) => (
           <div key={id} className="categories__item">
-          {/* <img srcset="img/categories/burger-sm.jpg 1x, img/categories/burger.jpg 2x" alt="Photo 1" className="categories__photos"> */}
+            <img className="categories__photos" srcSet={`./img/categories/${img} 1x, ./img/categories/${imgSm} 2x `}  alt={title} /> 
           <div className="categories__body">
               <h2>{title}</h2>
               <p>Shop Now</p>
