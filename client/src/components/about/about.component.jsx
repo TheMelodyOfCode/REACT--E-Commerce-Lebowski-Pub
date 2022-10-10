4867cc746f57bf65ab1892e88c22ccbdc536ef2b
D.Cameron <lobbygow@posteo.de>
2022-10-10 10:33:50 +0200

    fixed typo

--- client/src/components/about/about.component.jsx
+++ client/src/components/about/about.component.jsx
@@ -11,7 +11,7 @@ const About = ()=> {
                         <p>
                             Look, let me explain something to you.<br/> I'm not Mr. Lebowski.
                             - You're Mr. Lebowski. <br/>  I'm the Dude. So that's what you call me. 
-                            That, or His Dudeness … Duder … or El Duderino, if, you know, you're not into the whole brevity thing. 
+                            That, or His Dudeness … Duder … or El Duderino, if, you know .., you're not into the whole brevity thing. 
                         </p>
                     </div>
                 </div>
