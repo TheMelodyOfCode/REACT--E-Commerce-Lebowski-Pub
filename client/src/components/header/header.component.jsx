4ddc3dc1e162f227e6faae4e5e8c465ac6473e93
D.Cameron <lobbygow@posteo.de>
2022-10-10 10:34:20 +0200

    added fragment

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -1,17 +1,19 @@
 import {Outlet} from 'react-router-dom';
+import { Fragment } from "react";
+
 import Navigation from "../../routes/navigation/navigation.component";
 import About from '../about/about.component';
 
 const Header = ()=>{
 
     return (
-        <div>
-        <header className="header">
-            <Navigation />
-        </header>
-        <About/>
-        <Outlet />
-        </div>
+        <Fragment>
+            <header className="header">
+                <Navigation />
+            </header>
+            <About/>
+            <Outlet />
+        </Fragment>
     )
 
 }
