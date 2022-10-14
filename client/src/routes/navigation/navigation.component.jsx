0e018eadb189e9add1bd0a2594a58eb774a09f3e
D.Cameron <lobbygow@posteo.de>
2022-10-14 15:24:14 +0200

    fixed /auth route

--- client/src/routes/navigation/navigation.component.jsx
+++ client/src/routes/navigation/navigation.component.jsx
@@ -15,26 +15,19 @@ const Navigation = ()=> {
                     <Link to='/'><img className="navigation__photo" src="img/navLogo-64.png" alt="dude"/></Link>
                     <Link to='/' className="navigation__link navigation__logo">The Big Lebowski</Link>
                     <ul className="navigation__list">
-
-
-
-        { 
-        currentUser ? (
-        <li className="navigation__item"><Link className="navigation__link" onClick={signOutUser} > SIGN OUT</Link></li>
-            ) : (         
-            <li className="navigation__item">
-            <Link to='auth' className="navigation__link">Log-In</Link>
-            </li>
-            )} 
-
-
-    <li className="navigation__item"><Link to='shop' className="navigation__link">Shop</Link></li>
+                    { 
+                        currentUser ? (
+                        <li className="navigation__item"><Link className="navigation__link" onClick={signOutUser} > SIGN OUT</Link></li>
+                        ) : (         
+                        <li className="navigation__item">
+                        <Link to='/auth' className="navigation__link">Log-In</Link>
+                        </li>
+                    )} 
+                        <li className="navigation__item"><Link to='shop' className="navigation__link">Shop</Link></li>
                     </ul>
                 </nav>
             </div> 
         </Fragment>
-           
-
     );
   }
 
