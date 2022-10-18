372c0f4a3f35969d60b76f3f9f215f9325182fb4
D.Cameron <lobbygow@posteo.de>
2022-10-18 15:26:30 +0200

    created shop products data file for db

---
+++ client/src/shop-data.js
@@ -0,0 +1,64 @@
+const SHOP_DATA = [
+  {
+    title: 'Burger',
+    items: [
+      {id:1,title:'Ultimate Burger',img:'burger1.jpg',imgSm:'burger1-sm.jpg',price:12 ,route:'/burger'},
+      {id:2,title:'Maniac Burger',img:'burger2.jpg',imgSm:'burger2-sm.jpg',price:16,route:'/burger'},
+      {id:3,title:'Madness Burger',img:'burger3.jpg',imgSm:'burger3-sm.jpg',price:8,route:'/burger'},
+      {id:4,title:'Crazy Burger',img:'burger4.jpg',imgSm:'burger4-sm.jpg',price:10,route:'/burger'},
+      {id:5,title:'Smokey Burger',img:'burger5.jpg',imgSm:'burger5-sm.jpg',price:11,route:'/burger'},
+      {id:6,title:'Saddam Burger',img:'burger6.jpg',imgSm:'burger6-sm.jpg',price:7.50,route:'/burger'},
+      {id:7,title:'Bunny Burger',img:'burger7.jpg',imgSm:'burger7-sm.jpg',price:17,route:'/burger'}
+    ],
+  },
+  {
+    title: 'Whiskey',
+    items: [
+      {id:1,title:'Super Whiskey',img:'whiskey1.jpg',imgSm:'whiskey1-sm.jpg',price:18,route:'/whiskey'},
+      {id:2,title:'Awesome Whiskey',img:'whiskey2.jpg',imgSm:'whiskey2-sm.jpg',price:12 ,route:'/whiskey'},
+      {id:3,title:'Ultimate Whiskey',img:'whiskey3.jpg',imgSm:'whiskey3-sm.jpg',price:10,route:'/whiskey'},
+      {id:4,title:'Deluxe Whiskey',img:'whiskey4.jpg',imgSm:'whiskey4-sm.jpg',price:23,route:'/whiskey'},
+      {id:5,title:'The Woo Whiskey',img:'whiskey5.jpg',imgSm:'whiskey5-sm.jpg',price:25,route:'/whiskey'},
+      {id:6,title:'The Jeffrey Whiskey',img:'whiskey6.jpg',imgSm:'whiskey6-sm.jpg',price:14,route:'/whiskey'},
+      {id:7,title:'The Cab Driver Whiskey',img:'whiskey7.jpg',imgSm:'whiskey7-sm.jpg',price:9,route:'/whiskey'}
+    ],
+  },
+  {
+    title: 'White Russians',
+    items: [
+      {id:1,title:'The Original',img:'cocktail1.jpg', imgSm:'cocktail1-sm.jpg',price:10,route:'/cocktails'},
+      {id:2,title:'The Walter',img:'cocktail2.jpg',imgSm:'cocktail2-sm.jpg',price:8 ,route:'/cocktails'},
+      {id:3,title:'The Donney',img:'cocktail3.jpg',imgSm:'cocktail3-sm.jpg',price:7,route:'/cocktails'},
+      {id:4,title:'The Jesus',img:'cocktail4.jpg',imgSm:'cocktail4-sm.jpg',price:15,route:'/cocktails'},
+      {id:5,title:'The Gutter Ball',img:'cocktail5.jpg',imgSm:'cocktail5-sm.jpg',price:17,route:'/cocktails'},
+      {id:6,title:'The Police Chief',img:'cocktail6.jpg',imgSm:'cocktail6-sm.jpg',price:9,route:'/cocktails'},
+      {id:7,title:'The DUDE',img:'cocktail7.jpg',imgSm:'cocktail7-sm.jpg',price:21,route:'/cocktails'}
+    ],
+  },
+  {
+    title: 'Bowling Balls',
+    items: [
+      {id:1,title:'The Antidote',img:'ball1.jpg',imgSm:'',price:25 ,route:'/bowlingballs'},
+      {id:2,title:'Path Rising',img:'ball2.jpg',imgSm:'',price:20 ,route:'/bowlingballs'},
+      {id:3,title:'Idol Helios',img:'ball3.jpg',imgSm:'',price:35 ,route:'/bowlingballs'},
+      {id:4,title:'Nova',img:'ball4.jpg',imgSm:'',price:40,route:'/bowlingballs'},
+      {id:5,title:'RST 3 X',img:'ball5.jpg',imgSm:'',price:60,route:'/bowlingballs'},
+      {id:6,title:'ZONE',img:'ball6.jpg',imgSm:'',price:58,route:'/bowlingballs'},
+      {id:7,title:'ICE',img:'ball7.jpg',imgSm:'',price:57,route:'/bowlingballs'}
+    ],
+  },
+  {
+    title: 'Rugs',
+    items: [
+      {id:1,title:'Classic Rug',img:'rug1.jpg',imgSm:'rug1-sm.jpg',price:25,route:'/rugs'},
+      {id:2,title:'Vintage Rug',img:'rug2.jpg',imgSm:'rug2-sm.jpg',price:55,route:'/rugs'},
+      {id:3,title:'Retro Rug',img:'rug3.jpg',imgSm:'rug3-sm.jpg',price:45,route:'/rugs'},
+      {id:4,title:'Fart Rug',img:'rug4.jpg',imgSm:'rug4-sm.jpg',price:33,route:'/rugs'},
+      {id:5,title:'Stranger Rug',img:'rug5.jpg',imgSm:'rug5-sm.jpg',price:37,route:'/rugs'},
+      {id:6,title:'Nihilist Rug',img:'rug6.jpg',imgSm:'rug6-sm.jpg',price:7,route:'/rugs'},
+      {id:7,title:'The Da Fino Rug',img:'rug7.jpg',imgSm:'rug7-sm.jpg',price:19,route:'/rugs'}
+    ],
+  },
+];
+
+export default SHOP_DATA;
