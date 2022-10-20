5546f1308f55611e81b5d99673520d17776348f4
D.Cameron <lobbygow@posteo.de>
2022-10-20 09:44:36 +0200

    fixed styling issues for mobile

--- client/src/routes/navigation/navigation.component.jsx
+++ client/src/routes/navigation/navigation.component.jsx
@@ -17,16 +17,16 @@ const Navigation = ()=> {
         <Fragment> 
             <div className="navigation">
                 <nav className="navigation__nav">
-                    {/* <Link to='/'><NavIcon className="navigation__photo"  alt="dude"/></Link> */}
-                    <Link to='/'><img className="navigation__photo" src="./img/navLogo-64.png" alt="dude"/></Link>
+                    <Link to='/'><img className="navigation__photo" src="img/navLogo-64.png" alt="dude"/></Link>
                     <Link to='/' className="navigation__link navigation__logo">The Big Lebowski</Link>
+                    <Link to='/' className="navigation__link navigation__logo-sm">Lebowski</Link>
                     <ul className="navigation__list">
                     { 
                         currentUser ? (
                         <li className="navigation__item"><Link className="navigation__link" onClick={signOutUser} > SIGN OUT</Link></li>
                         ) : (         
                         <li className="navigation__item">
-                        <Link to='/auth' className="navigation__link">Log-In</Link>
+                        <Link to='/auth' className="navigation__link">LogIn</Link>
                         </li>
                     )} 
                         <li className="navigation__item"><Link to='/shop' className="navigation__link">Shop</Link></li>
