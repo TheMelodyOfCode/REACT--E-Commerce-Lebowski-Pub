d9927f48031c205706d4e0c5c6ffa83774fd3ebf
D.Cameron <lobbygow@posteo.de>
2022-10-20 11:21:54 +0200

    fixed btn className

--- client/src/components/product-card/product-card.component.jsx
+++ client/src/components/product-card/product-card.component.jsx
@@ -18,7 +18,6 @@ return (
                 <span className='name'>{title}</span>
                 <span className='price'>{price}</span>
             </div>   
-            {/* <Button className='btn shopItems__button'  type="submit" onClick={addProductToCart} >ADD to Cart</Button>    */}
             <Button btnType='shop'  type="submit" onClick={addProductToCart} >ADD to Cart</Button>   
     </div>
 );
