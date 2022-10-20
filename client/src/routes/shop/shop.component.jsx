d5030ff678055e82902b31d4f83537945e17d6fa
D.Cameron <lobbygow@posteo.de>
2022-10-20 10:35:27 +0200

    fixed styling issues for mobile

--- client/src/routes/shop/shop.component.jsx
+++ client/src/routes/shop/shop.component.jsx
@@ -2,9 +2,7 @@
 import { Fragment } from 'react';
 import {Routes, Route} from 'react-router-dom';
 
-
 import Navigation from '../navigation/navigation.component';
-// import CategoryPreview from '../../components/category-preview/category-preview.component';
 import CategoriesPreview from '../../routes/categories-preview/categories-previews.component'
 import Category from '../category/category.component';
 import Footer from '../footer/footer.component';
